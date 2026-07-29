8ca8834a6fdce8fcc24edccccf3c423aa7cc3f57
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2026-07-28 20:33:16 -0400

    V23.3 version start: SHYLAND_VERSION 23.3-DEV, pin test moved (implementation)

--- django/src/apps/shyland/tests/test_b2_amendment1.py
+++ django/src/apps/shyland/tests/test_b2_amendment1.py
@@ -115,7 +115,7 @@ class VersionLineTests(TransactionTestCase):
         self.assertEqual(lines[-2], {})
         # The pin moves with the version rituals: version start sets
         # N.0-DEV, the closeout ritual sets the release stamp (N.0).
-        self.assertEqual(SHYLAND_VERSION, '23.2')
+        self.assertEqual(SHYLAND_VERSION, '23.3-DEV')
 
 
 class CategoryRetagTests(TransactionTestCase):
